d329a2e4da584f93329a3775ceadddf39fee65cc
Altermanns <isaac.277.trujillo@gmail.com>
2024-10-23 16:35:49 -0500

    Modelos

---
+++ Proyecto/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto.Models
+{
+    public class Usuario
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; }
+        [Required]
+        //[DataType]
+        public DateOnly FechaNacimiento { get; set; }
+        
+    }
+}
---
+++ Proyecto/Models/Vehiculo.cs
@@ -0,0 +1,12 @@
+﻿namespace Proyecto.Models
+{
+    public class Vehiculo
+    {
+        public int IdVehiculo {  get; set; }
+        public string Marca  { get; set; }
+        public string Modelo { get; set; }
+        public DateOnly Año { get; set; }
+        public float precio { get; set; }
+        public float kilometraje { get; set; }
+    }
+}
